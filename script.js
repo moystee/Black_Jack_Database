6b2459a82b32e8783a27a6bfcee3de1e0fbb89cf
Jared Moy <jmoy02@calpoly.edu>
2026-06-16 19:34:34 -0700

    Add event listeners for start and join game buttons

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// Add javascript code hre
+const startButton = document.getElementById("startGameButton");
+const joinButton = document.getElementById("joinGameButton");
+const message = document.getElementById("message");
+
+startButton.addEventListener("click", function () {
+    message.textContent = "Starting game...";
+});
+
+joinButton.addEventListener("click", function () {
+    message.textContent = "No existing games found. Please start a new game.";
+});
