d6f817c2036668384a45a82417d4771810bdbc46
Jared Moy <jmoy02@calpoly.edu>
2026-06-17 16:48:53 -0700

    Add button event listeners for game actions

--- js/game.js
+++ js/game.js
@@ -1,1 +1,22 @@
-// add game js code here
+const hitButton = document.getElementById("hitButton");
+const standButton = document.getElementById("standButton");
+const splitButton = document.getElementById("splitButton");
+const exitButton = document.getElementById("exitButton");
+
+const gameMessage = document.getElementById("gameMessage");
+
+hitButton.addEventListener("click", function () {
+    gameMessage.textContent = "Hit button clicked.";
+});
+
+standButton.addEventListener("click", function () {
+    gameMessage.textContent = "Stand button clicked.";
+});
+
+splitButton.addEventListener("click", function () {
+    gameMessage.textContent = "Split button clicked.";
+});
+
+exitButton.addEventListener("click", function () {
+    gameMessage.textContent = "Exit button clicked.";
+});
